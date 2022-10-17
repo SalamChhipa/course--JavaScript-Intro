62798a5b576a165769b3987f8859cd16b4fc9d62
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-17 22:37:20 +0530

    third file added

---
+++ js/3.comments-in-JS.js
@@ -0,0 +1,6 @@
+document.write("two types of comments:<br>");
+document.write("single line<br>Multi line<br>");
+document.write("you should run the console to see  comment codes as it is ignored in runtime process");
+//this is an example of single line comment
+/*this is an example of
+multi line comments*/
