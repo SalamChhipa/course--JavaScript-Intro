3f0473e8c0168ee9c20011be4def407442537d00
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-18 01:04:01 +0530

    fifth file added in folder

---
+++ js/5.Data-types-in-JS.js
@@ -0,0 +1,21 @@
+var a="salam chhipa";//string
+var b=100;//number
+var c="100";//number as a string
+var d=10.3;//float number
+var e=true;// boolean
+var f=false;//boolean false
+var g=['salam',7062,'chhipa'];//array
+var h={class1:"salam",class2:"chhipa"};//object
+var i=null;//null means take memory space with no data
+var j;//undefined
+document.write("every data type is printed once. use console to better understand JS file.<br>");
+document.write(a,'<br>');
+document.write(b,"<br>");
+document.write(c,"<br>");
+document.write(d,'<br>');
+document.write(e,"<br>");
+document.write(f,"<br>");
+document.write(g,"<br>");
+document.write(h,"<br>");
+document.write(i,"<br>");
+document.write(j);
