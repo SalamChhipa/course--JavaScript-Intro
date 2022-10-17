d7cf64c6d9135c11a7382ca6306dcf6b69fa31f2
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-18 02:36:34 +0530

    seventh file added in folder

---
+++ js/7.Assignment-operators-in-JS.js
@@ -0,0 +1,20 @@
+document.write("use console to better understand the code<br>");
+document.write('x="10",y="5"<br>');
+var x=10,y=5;
+document.write("x+=y:<br>");
+document.write(x+=y,"<br>");
+x=10
+document.write("x-=y:<br>");
+document.write(x -= y,"<br>");
+x=10
+document.write("x*=y:<br>");
+document.write(x*=y,"<br>");
+x=10
+document.write("x/=y:<br>");
+document.write(x/=y,"<br>");
+x=10
+document.write("x%=y:<br>");
+document.write(x%=y,"<br>");
+x=10
+document.write("x**=y:<br>");
+document.write(x**=y);
