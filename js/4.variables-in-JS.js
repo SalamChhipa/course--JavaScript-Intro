c40e20991df01546cf27d63c21c6d6b7f3c7e707
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-17 23:22:41 +0530

    files for variables added

---
+++ js/4.variables-in-JS.js
@@ -0,0 +1,16 @@
+document.write("all three:<br>");
+document.write('var<br>');
+document.write("let<br>");
+document.write('const<br>');
+//var(multiple declare and  value assignment)
+var a=100;
+a=30;
+var a=50;
+//let(single declare and multiple value assignment)
+let b="salam chhipa";
+b="salam tak";
+//const(single declare and value assignment)
+const c=undefined;
+document.write(a,' (exaple of var)<br>');
+document.write(b,' (example of let)<br>');
+document.write(c,' (example of const)<br>');
