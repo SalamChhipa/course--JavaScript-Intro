bee77615c5929961d7e03f1e86aa44c448e42eee
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-18 20:15:09 +0530

    eighth file uploaded in folder

---
+++ js/8.Comparison-operators-in-JS.js
@@ -0,0 +1,15 @@
+document.write("few examples:<br>");
+document.write("results of examples as listed as table order <br>");
+document.write("a=5 b=5(but a string)<br>");
+let a,b;
+a=5;
+b="5";
+document.write(a==b,"<br>");//answer will be true
+document.write(a===b,"<br>");//answer will be false because data types are different
+document.write(a!=b,"<br>");//answer will be false
+document.write(a!==b,"<br>");//answer wil be true because data types are different
+document.write(a>b,"<br>");
+document.write(a<b,"<br>");
+document.write(a>=b,"<br>");
+document.write(a<=b,"<br>");
+document.write("use console to better understand code");
