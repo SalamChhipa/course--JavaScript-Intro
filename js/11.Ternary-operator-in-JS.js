082437791add9f05677b19d80cb7a236cbf66d17
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-20 02:18:57 +0530

    eleventh file is added

---
+++ js/11.Ternary-operator-in-JS.js
@@ -0,0 +1,11 @@
+//only a ternary operato
+//(condition)?true:false
+document.write("a=10<br>");
+var a=10;
+
+(a==10)? document.write("luckily a matches<br>"): document.write("unmatched,bad luck :(<br>")
+ //using ternary's result as a value  of variable
+ var b;
+ b= (a == 10)? 5 : 0
+document.write(b,"if it was unmatched b was not exist<br>");
+document.write("console is best friend of JavaScript");
