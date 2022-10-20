fb99f74ad8f6ac75db6c8e1095230010ae2a8e1b
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-20 23:09:17 +0530

    twelvth file is added

---
+++ js/12.Switch-case-in-JS.js
@@ -0,0 +1,23 @@
+document.write('a=3<br>');
+var a=3;
+    switch(a){
+        case 0 :
+            document.write("reached at 0<br>");
+            break;
+        case 1 :
+            document.write("reached at 1<br>");
+        break;
+        case 2:
+        document.write("reached at 3<br>");
+        break;
+        case 3 :
+            document.write("reached a 3<br>");
+        break;
+        case 4 :
+            document.write("reached at 4<br>");
+        break;
+        case 5 :
+            document.write("reachedat 5<br>");
+        default : document.write("not a valid number<br>");
+
+    }
