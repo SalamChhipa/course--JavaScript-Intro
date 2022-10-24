27f1abe1e16281cf06f94f8fe959bbd8bb194aab
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-24 22:56:12 +0530

    twentieth file is added

---
+++ js/20.For-loop-in-js.js
@@ -0,0 +1,5 @@
+var a = "salam chhipa"
+for(b=1;b<=10;b++){
+    document.write(a,"<br>");//the initialization the condition and the increment part all in a single line
+}
+document.write("thnx for visit use the console to get the js code");
