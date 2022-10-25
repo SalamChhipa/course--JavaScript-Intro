c4d104c31b48a1575e03140d3452c337282852dc
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-25 23:34:25 +0530

    twentyoneth file is added

---
+++ js/21.Break-and-continue-statement.js
@@ -0,0 +1,11 @@
+let a;
+for(a = 1; a<=10; a++){
+    if(a == 5){
+        document.write("salam tak<br>");
+        continue;
+    }else if(a==8){
+        document.write("abdul salam chiipa");
+        break;
+    }
+    document.write("salam chhipa<br>");
+}
