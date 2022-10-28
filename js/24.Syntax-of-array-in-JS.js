e8e84631ad7edc73a6ea9ef74a58c4cc76610d80
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-28 23:05:14 +0530

    twenty fourth file is added

---
+++ js/24.Syntax-of-array-in-JS.js
@@ -0,0 +1,7 @@
+let array = ["salam chhipa",21,"alive"];
+document.write('printing without for loop : <br>');
+document.write(array,"<br><br>");
+document.write("printing using for loop : <br>");
+for (let a=0;a < array.length;a++){
+    document.write(array[a] + "<br>");
+}
