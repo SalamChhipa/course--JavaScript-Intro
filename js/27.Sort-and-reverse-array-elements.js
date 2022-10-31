6e0f5cd480db28fe31aa05411b4ad5e497119922
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-31 21:01:27 +0530

    twenty seventh file is added

---
+++ js/27.Sort-and-reverse-array-elements.js
@@ -0,0 +1,9 @@
+let array  = ["salam","yasir","amaan","umer","rehman","ibrahim","rahul","deepak"];
+document.write("let print the array that this file contains: <br> <br>");
+document.write(array,"<br> <br>i need this array alphabatic sorted: <br> <br>");
+array.sort();//sorting in alphabetical
+document.write(array,"<br><br>");
+array = [0,1,2,3,4,5,6,7];
+document.write("lets print a new array <br><br>",array,"<br><br>");
+array.reverse();//reversing array elements
+document.write("reversing array elements<br><br>",array);
