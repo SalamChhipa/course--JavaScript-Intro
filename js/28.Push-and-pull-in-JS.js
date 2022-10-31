05b12900b422c8cb688ce79942094fc23e294617
SalamChhipa <abdulsalamtak55@gmail.com>
2022-10-31 21:50:01 +0530

    twenty eigth file is added

---
+++ js/28.Push-and-pull-in-JS.js
@@ -0,0 +1,8 @@
+let array = ["salam","chhipa","tak",21,"graduate"];
+document.write("<br>",array,"<br><br>");
+document.write("lets pull out graduate from this array<br>");
+array.pop();//pulling last elements
+document.write(array,"<br><br>");
+document.write("lets add B.Scin that last index<br><br>");
+array.push("B.Sc");//pushing at last eements
+document.write(array);
