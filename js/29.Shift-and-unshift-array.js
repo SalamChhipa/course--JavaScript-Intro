b7a54fbf01cebee190b93bccb6f85501a12dafab
SalamChhipa <abdulsalamtak55@gmail.com>
2022-11-01 18:05:37 +0530

    twenty ninth file is added

---
+++ js/29.Shift-and-unshift-array.js
@@ -0,0 +1,6 @@
+let array = ["mustaq","tak",21,"b.sc final year"];
+document.write(array,"<br>lets remove mustaq<br>");
+array.shift();//removed mustaq element of index 0
+document.write("after array<br>",array,"<br>");
+array.unshift("salam");//salam is added to index 0
+document.write("now lets add salam in that<br>",array,"<br>salam is added");
