1cd98a507629244ac7966aa4ce0c3554e85864a7
SalamChhipa <abdulsalamtak55@gmail.com>
2022-11-03 22:48:12 +0530

    thirty two file is added

---
+++ js/32.Use-of-isArray-in-JS.js
@@ -0,0 +1,5 @@
+var a = ["salam","tak","chhipa"],b="alive",c;
+c = Array.isArray(a);
+document.write("check if a is an array<br><br>",c,"<br><br>");// will give true
+c = Array.isArray(b);
+document.write("lets see if b is an array<br><br>",c);
