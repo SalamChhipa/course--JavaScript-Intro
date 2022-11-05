7465bd8adf6eb7da86b6e93f27aa04ed00f16572
SalamChhipa <abdulsalamtak55@gmail.com>
2022-11-05 20:52:22 +0530

    thirty three file is added

---
+++ js/33.Use-of-indexOf-andlastindexOf-in-array.js
@@ -0,0 +1,6 @@
+let array = ["salam","tak","chhipa",21];
+let b = array.indexOf("tak",0);
+document.write(b,"<br>");
+document.write('now lets print index number of "chhipa" with lastindexOf :<br>');
+b = array.lastIndexOf("chhipa",3);
+document.write(b);
