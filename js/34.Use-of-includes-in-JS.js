262ae16c575d92019b6a0c02bcf5dc55efd19e88
SalamChhipa <abdulsalamtak55@gmail.com>
2022-11-06 23:57:08 +0530

    thirty fourth file is added

---
+++ js/34.Use-of-includes-in-JS.js
@@ -0,0 +1,5 @@
+let array = ["salam","tak","chhipa"];
+let b = array.includes("salam");
+document.write(b,"<br> lets see if 'mustaq' exists or not<br> ");
+b= array.includes("mustaq");
+document.write(b);
